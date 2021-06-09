51001d87fc8a07f1749e1bc89fedd7f98065e405
Tarun Sankar <53414760+mecoding4fun@users.noreply.github.com>
2021-06-09 17:57:40 +0530

    Add files via upload

---
+++ C107.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.express as px
+import csv
+import math
+import plotly.graph_objects as go
+
+
+df = pd.read_csv('Data3.csv')
+# print(df.groupby('level')['attempt'].mean())
+# print(p)
+# f = go.Figure(go.Bar(x=p.groupby('level')['attempt'].mean(),
+#     y=['Level 1','Level 2','Level 3','Level 4'],orientation='h'))
+# f.show()
+mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
+f = px.scatter(mean, x="student_id", y="level", size="attempt")
+f.show()
+# print(df.groupby('student_id')['level'].mean())
